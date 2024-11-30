5b743dea802001b6bbb47f19e0eb99e444a34a2e
ben3345678 <q20038848@gmail.com>
2024-11-30 17:08:29 +0800

    fix: 修正 選單點選

--- components/BottomNav.js
+++ components/BottomNav.js
@@ -2,10 +2,16 @@ import Link from "next/link";
 
 export default function BottomNav() {
   return (
-    <div className="fixed bottom-0 left-0 right-0 bg-gray-800 flex justify-around py-3 z-50 text-white">
-      <Link href="/">菜單</Link>
-      <Link href="/statistics">統計</Link>
-      <Link href="/manage">管理</Link>
+    <div className="fixed bottom-0 left-0 right-0 bg-gray-800 flex justify-around py-3 z-50 text-white text-center">
+      <Link href="/" className="w-full">
+        菜單
+      </Link>
+      <Link href="/statistics" className="w-full">
+        統計
+      </Link>
+      <Link href="/manage" className="w-full">
+        管理
+      </Link>
     </div>
   );
 }
