9838296135fdf0b3305b4a2930abc9f094cd0781
ben3345678 <q20038848@gmail.com>
2024-11-30 16:47:32 +0800

    fix: 修正字體大小

--- src/app/page.js
+++ src/app/page.js
@@ -78,7 +78,7 @@ export default function MenuPage() {
 
   return (
     <div className="p-4 max-w-screen-lg mx-auto">
-      <h1 className="text-2xl font-bold mb-6 text-center">飲料菜單</h1>
+      <h1 className="text-2xl font-bold mb-1 text-center">飲料菜單</h1>
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {drinks.map((item, index) => (
           <div
